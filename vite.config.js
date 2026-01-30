39b67c0026ef0459c7d4b80c490e00196f753672
Tudy006 <musattudor10@gmail.com>
2026-01-30 23:55:49 +0100

    Added base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite"; // <-- Import this
 
 export default defineConfig({
+  base: "/Guess-the-AI/", // Replace with your exact repo name
   plugins: [
     react(),
     tailwindcss(), // <-- Add this
